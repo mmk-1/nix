78bc0ebfba0f533e8200a69930c9332bfdaad40b
Mahdi Khosravi <mmk1776@gmail.com>
2023-12-16 15:18:04 +0300

    commenting out for later config

--- home.nix
+++ home.nix
@@ -1,28 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "mmk";
   home.homeDirectory = "/home/mmk";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-    pkgs.neovim
-    pkgs.zellij
+    # pkgs.neovim
+    # pkgs.zellij
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -38,38 +26,19 @@
     # '')
   ];
 
+  # programs.neovim = {
+  #   enable = true;
+  #   extraPackages = with pkgs; lib.attrsets.mapAttrsToList (name: value: pkgs.lib.getAttr value pkgs) (builtins.fromJSON (builtins.readFile ./config/nvim/lsp_servers.json));
+  # };
+
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-    ".config/nvim".source = ./config/nvim;
-    # ".config/nvim" = {
-    #     recursive = true;
-    #     source = ../../dotfiles/nvim;
-    # };
-    ".config/zellij".source = ./config/zellij;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    # ".config/nvim".source = ./config/nvim;
+    # ".config/zellij".source = ./config/zellij;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/mmk/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
